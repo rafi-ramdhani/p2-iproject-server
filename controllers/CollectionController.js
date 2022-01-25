498b5a79d921353506db77b52c360667094e237a
rafi-ramdhani <rafiramdhani20@gmail.com>
2022-01-25 19:26:51 +0700

    Also added get all collections features

--- controllers/CollectionController.js
+++ controllers/CollectionController.js
@@ -17,6 +17,47 @@ class CollectionController {
     }
   }
 
+  // Get all collections
+  static async getCollections(req, res, next) {
+    try {
+      let collectionsOutput = []
+
+      const collections = await Collection.findAll({
+        where: {
+          UserId: +req.currentUser.id
+        }
+      })
+
+      const collectionsWithCharacters = await axios.get("https://thronesapi.com/api/v2/Characters")
+
+      collections.forEach((el1, i) => {
+        collectionsWithCharacters.data.forEach((el2) => {
+          if (el1.fetchIdAPI === el2.id) {
+            collectionsOutput.push({
+              id: el1.id,
+              fetchIdAPI: el1.fetchIdAPI,
+              UserId: el1.UserId,
+              character: {
+                id: 0,
+                firstName: el2.firstName,
+                lastName: el2.lastName,
+                fullName: el2.fullName,
+                title: el2.title,
+                family: el2.family,
+                image: el2.image,
+                imageUrl: el2.imageUrl
+              }
+            })
+          }
+        })
+      })
+
+      res.status(200).json(collectionsOutput)
+    } catch (err) {
+      next(err)
+    }
+  }
+
 }
 
 module.exports = CollectionController

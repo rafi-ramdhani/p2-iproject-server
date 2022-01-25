91d936e93f0118e1bf647a5b28b6d95698ffbd33
rafi-ramdhani <rafiramdhani20@gmail.com>
2022-01-25 23:28:51 +0700

    Added authentication if access token is falsy

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -4,6 +4,11 @@ const { verifyToken } = require('../helpers/jwt')
 const authentication = async (req, res, next) => {
   try {
     const { access_token } = req.headers
+
+    if (!access_token) {
+      throw { name: "JsonWebTokenError" }
+    }
+
     const payload = verifyToken(access_token)
     const user = await User.findByPk(payload.id)
 
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   switch (err.name) {
     case "SequelizeValidationError":
-      res.status(400).json({ message: err.errors[0].message })
-      break;
-
     case "SequelizeUniqueConstraintError":
       res.status(400).json({ message: err.errors[0].message })
       break;
@@ -13,7 +10,15 @@ const errorHandler = (err, req, res, next) => {
       break;
 
     case "Insufficient":
-      res.status(401).json({ message: "Points is insufficient" })
+      res.status(40).json({ message: "Insufficient points" })
+      break;
+
+    case "Error":
+      res.status(404).json({ message: "Character not found" })
+      break;
+
+    case "JsonWebTokenError":
+      res.status(401).json({ message: "Invalid access" })
       break;
 
     default:
